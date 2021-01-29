6a570103fdeb027892636ae50b3c3e89fa9b9ffd
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2021-01-29 12:08:17 +0100

    ranme routes and views

--- Root.js
+++ Root.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Login from './src/pages/LoginView/Login';
-import Register from './src/pages/RegisterView/Register';
-import ForgotPassword from './src/pages/ForgotPasswordView/ForgotPassword';
-import Home from './src/pages/HomeView/Home';
-import Contact from './src/pages/ContactView/Contact';
+import Login from './src/pages/LoginView/';
+import Register from './src/pages/RegisterView/';
+import ForgotPassword from './src/pages/ForgotPasswordView/';
+import Home from './src/pages/HomeView/';
+import Contact from './src/pages/ContactView/';
 
 const Stack = createStackNavigator();
 
