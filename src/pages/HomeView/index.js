9d9f827fb2874a59023327114348be6340c7007f
Gwen <>
2021-01-29 12:48:14 +0100

    homeView

--- src/pages/HomeView/index.js
+++ src/pages/HomeView/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import { SafeAreaView } from 'react-native';
-import { Text } from 'react-native';
-
-=======
 import { SafeAreaView, Text } from 'react-native';
 
 import Contact from '../../components/Contact';
 
->>>>>>> d0d8259d02d172ae62b619e5c34f5beff9976be8
 const Home = () => {
     return (
         <SafeAreaView>
             <Text>Home</Text>
-<<<<<<< HEAD
-=======
             <Contact />
->>>>>>> d0d8259d02d172ae62b619e5c34f5beff9976be8
         </SafeAreaView>
     )
 }
