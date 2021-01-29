f75812dc7b84c52714ff51393bc3a587f36f6a78
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2021-01-29 11:57:01 +0100

    rename component

--- src/pages/ForgotPasswordView/ForgotPassword.js
+++ src/pages/ForgotPasswordView/ForgotPassword.js
@@ -1,7 +1,41 @@
-const ForgotPassword = ({ navigation }) => {
+import React, { useState } from 'react';
+import { View, SafeAreaView, Text, TextInput, Button } from 'react-native';
+
+const ForgotPassword = (onPressForgotPassword) => {
+
+    const [username, setUsername] = useState('');
+
+    const [formState, setFormState] = useState({
+        message: '',
+        isLoggingIn: false
+    });
+
+    // const buttonState = formState.isLoggingIn || !user.username || !user.password;
+
+    // const handleChange = e => {
+    //     console.log(e.target);
+    //     setUser({ ...user, [e.target.id]: e.target.value });
+    // };
+
     return (
-        <>
-        </>
+        <SafeAreaView>
+            <View>
+                <Text>
+                    Forgot Password Page
+                </Text>
+                <TextInput
+                    placeholder='Username'
+                    onChangeText={(username => setUsername(username))}
+                    value={username}
+                />
+                <Button
+                    onPress={e => handleForgotPassword(e)}
+                    title='Reset password'
+                // disabled={buttonState}
+                />
+            </View>
+        </SafeAreaView>
+
     )
 }
 
