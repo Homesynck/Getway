2dc3a42164ca776daeaf2987cae55f7ec98a74a8
Danny-7 <daniel.aguiar@outlook.fr>
2021-01-29 22:43:30 +0100

    add: native module test

--- App.js
+++ App.js
@@ -2,24 +2,32 @@ import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 
 import Root from './Root';
-
+import { NativeModules } from 'react-native'
 
 const App = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const { Getway } = NativeModules;
+
+  const sayHiFromJava = async () => {
+    Getway.sayHi((err) => { console.log(err) }, (msg) => { console.log(msg) });
+  };
+
+  sayHiFromJava();
+
   return (
-      <Root />
-      // <StatusBar barStyle="dark-content" />
-      // <SafeAreaView>
-      //   <ScrollView
-      //     contentInsetAdjustmentBehavior="automatic">
-      //     <Header />
-      //     <View>
-      //       <Login />
-      //     </View>
-      //   </ScrollView>
-      // </SafeAreaView>
+    <Root />
+    // <StatusBar barStyle="dark-content" />
+    // <SafeAreaView>
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic">
+    //     <Header />
+    //     <View>
+    //       <Login />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 };
 
