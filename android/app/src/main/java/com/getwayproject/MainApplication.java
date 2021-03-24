e445ad156407490c80cdc38bdc20fc027ec8a8e0
Daniel Aguiar <daniel.aguiar@outlook.fr>
2021-03-24 22:20:37 +0100

    add : react native contacts library

--- android/app/src/main/java/com/getwayproject/MainApplication.java
+++ android/app/src/main/java/com/getwayproject/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -26,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
           packages.add(new GetwayRPackage());
           return packages;
         }
