64fe09ff4931f35bcee6cd2ac8ff02b3ba3f3ea0
hte <57845968+nivadada@users.noreply.github.com>
2021-03-30 20:43:42 +0200

    style dashboard without navigation

--- src/scenes/home/dashboard.view.js
+++ src/scenes/home/dashboard.view.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, SafeAreaView, View, ScrollView, TouchableOpacity } from 'react-native';
-import { Icon, Text } from '@ui-kitten/components';
+import { Divider, Icon, Text } from '@ui-kitten/components';
 import Student from '../../assets/student.svg'
 import { Avatar } from "react-native-elements";
 import Contacts from "react-native-contacts";
@@ -19,43 +19,77 @@ const Dashboard = ({navigation}) => {
         getAllContacts();
     },[]);
 
+    const MyAvatar = ({i, size}) => (
+      <Avatar
+        size={size}
+        rounded
+        title={contactsData[i].displayName.split(' ').map((name) => name[0]).join('').toUpperCase()}
+        activeOpacity={0.7}
+        containerStyle={{ margin:10, backgroundColor: '#C1AB9A'}}
+      />
+    )
+
     return (
             <SafeAreaView>
-                <View style={{alignItems:'center', marginTop:10}}>
+                <View style={styles.image}>
                    <Student height={290} width={250}/>
-                   <Text category='h2' style={styles.text}>Mon Dashboard</Text>
+                   <Text style={styles.title}>MON DASHBOARD</Text>
                 </View>
-                <View style={{flexDirection:'row', justifyContent:'center'}}>
-                    <View style={[styles.container, styles.box, {justifyContent:'center', alignItems:'center'}]} >
+
+                <View style={[{flexDirection:'row'}, styles.center] }>
+                    <View style={[styles.container, styles.box, styles.center]} >
                         <Text style={styles.number}>{contactsData.length}</Text>
                         <Text category='s1' style={{color:'#E46F4C'}}>contacts</Text>
                     </View>
-                    <TouchableOpacity  style={[styles.container, styles.box]}>
-                        <Avatar
-                        size="large"
-                        rounded
-                        title={contactsData[0].displayName.split(" ").map((name) => name[0]).join('').toUpperCase()}
-                        activeOpacity={0.7}
-                        containerStyle={{ backgroundColor: '#F0DFCF' }}
-                        /> 
-                        <View>
-                            <Text>{contactsData[0].displayName}</Text>
-                            <Text>{contactsData[0].phoneNumbers[0].number}</Text>
-                        </View>
-                    </TouchableOpacity>
+                  <TouchableOpacity 
+                    //TODO onPress() 
+                    style={[styles.container, styles.box, styles.center]}
+                  >
+                    <MyAvatar i={0} size={'medium'}/>
+                    <View>
+                      <Text category='s1' style={{color:'#C1AB9A'}}>{contactsData[0].displayName}</Text>
                     </View>
-                    <View style={{justifyContent:'center', alignSelf:'center'}}>
+                  </TouchableOpacity>
+               </View>
+
+                <View style={styles.center}>
                     <View style={[styles.container, styles.long_box]}>
+                      <TouchableOpacity 
+                        //TODO onPress()
+                        style={{flexDirection:"row"}}
+                      >
+                        <MyAvatar i={1} size={'small'}/>
+                        <View style={styles.center}>
+                          <Text category='s1' style={styles.text}>{contactsData[1].displayName}</Text>
+                        </View>
+                      </TouchableOpacity>
+                      <Divider/>
+                      <TouchableOpacity 
+                        //TODO onPress()
+                        style={{flexDirection:"row"}}
+                      >
+                        <MyAvatar i={1} size={'small'}/>
+                        <View style={styles.center}>
+                          <Text category='s1' style={styles.text}>{contactsData[2].displayName}</Text>
+                        </View>
+                      </TouchableOpacity>
                     </View>
-                </View>
+                  </View>
             </SafeAreaView>
     )
 }
-                    {/* <TouchableOpacity 
-                        style={[styles.card, styles.contactListCard]} 
-                        onPress={() => navigation.navigate('Contacts')}>
-                    </TouchableOpacity> */}
+
 const styles = StyleSheet.create({
+    image:{
+      alignItems:'center',
+      marginTop:10
+    },
+    title:{
+      fontWeight:'100',
+      fontSize:24,
+      letterSpacing:8,
+      color:'#E46F4C'
+    },
     container:{
         padding:10,
         margin:13,
@@ -69,26 +103,28 @@ const styles = StyleSheet.create({
         shadowRadius: 1,
         backgroundColor:'white',
         elevation: 15,
-        
     },
     box: {
         height: 135,
         width: 135,
     },
+    center:{
+      justifyContent:'center', 
+      alignItems:'center'
+    },
     long_box: {
         height: 120,
         width: 300,
-    },
-    text:{
-        
-        letterSpacing:8,
-        color:'#C1AB9A'
     },
     number: {
         textAlign:'center',
         fontSize:50,
         fontWeight:'bold', 
         color:'#E46F4C'
+    },
+    text:{
+      color:'#24190F', 
+      letterSpacing:2
     }
 });
 
