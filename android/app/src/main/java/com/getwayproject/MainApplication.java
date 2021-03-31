a9b009925c600636b507240ed97b728dcfd6533d
hte <57845968+nivadada@users.noreply.github.com>
2021-03-31 09:53:44 +0200

    add icon

--- android/app/src/main/java/com/getwayproject/MainApplication.java
+++ android/app/src/main/java/com/getwayproject/MainApplication.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
